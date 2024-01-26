dff3240ac353c39d9722592fcdb6c7605852ab7a
JOHN HICKEY <2302441@uad.ac.uk>
2024-01-26 21:36:43 +0000

    MainMenu
    added start page

---
+++ Global Game Jam 2024/Assets/MenuController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+
+    public void begin()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+
+    public void Quit()
+    {
+#if UNITY_STANDALONE
+        Application.Quit();
+#endif
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+
+}
