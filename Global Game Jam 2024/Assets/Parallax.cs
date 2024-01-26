7b6622bb939d6cbe5acd8efbeab0543b00ca3aa1
UAD\2300318 <2300318@uad.ac.uk>
2024-01-26 22:53:56 +0000

    Parallax Final?
    Synced with Level Controller

--- Global Game Jam 2024/Assets/Parallax.cs
+++ Global Game Jam 2024/Assets/Parallax.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 //To be attached to all child parallax layers
 public class Parallax : MonoBehaviour
 {
+
     private float parallaxSpeed; //Layer dependent - units per frame
-    private float parallaxMultiplier;
+    private float moveSpeed;
     private float spriteLength;
 
     void Start() {
-        
+
         //Layers:
         //Sky - 6
         //Background - 7
         //Road - 8
         //Foreground - 9
         parallaxSpeed = (float)(((float)gameObject.layer - 5)/4 + 0.25); //Range transformation [6,9] -> [1,4] -> [0.25,1] -> [0.5,1.25] (This transformation ensures that the road layer has a parallax speed of 1)
-        parallaxMultiplier = 0.05f;
+        moveSpeed = 100f;
         Debug.Log((gameObject.layer - 5)/4);
         spriteLength = GetComponent<SpriteRenderer>().bounds.size.x;
     }
@@ -26,7 +27,7 @@ public class Parallax : MonoBehaviour
         if (transform.position.x < -(spriteLength)) //Out of bounds
             Destroy(gameObject);
 
-        Vector3 deltaPos = new Vector3(parallaxSpeed * parallaxMultiplier, 0, 0);
+        Vector3 deltaPos = new Vector3(parallaxSpeed * LevelController.Instance.speedMultiplier * moveSpeed * Time.deltaTime, 0, 0);
         transform.position -= deltaPos;
     }
 }
