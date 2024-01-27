0c9735deeb5f532181730fbf0d9a5102c83c231e
Serpendio <58090288+Serpendio@users.noreply.github.com>
2024-01-27 20:49:10 +0000

    Multiple spawns

--- Global Game Jam 2024/Assets/Scripts/Scene/SpawningAlternative.cs
+++ Global Game Jam 2024/Assets/Scripts/Scene/SpawningAlternative.cs
@@ -4,14 +4,45 @@ using UnityEngine;
 
 public class SpawningAlternative : SpawningBase
 {
-    [SerializeField] GameObject obstacle;
+    [SerializeField] GameObject[] obstacles;
+    [SerializeField] int[] obstacleSpawnChances;
+    [SerializeField] GameObject[] powerups;
+    [SerializeField] int[] powerupSpawnChances;
 
     protected override void Update()
     {
         base.Update();
         if (spawnTimer <= 0)
         {
-            SpawnObject(obstacle, Random.Range(LevelController.Instance.roadSize, -LevelController.Instance.roadSize));
+            GameObject objToSpawn;
+            int ran = Random.Range(0, 100);
+            if (Random.Range(0, 100) < 5) // powerup
+            {
+                objToSpawn = powerups[^1];
+                for (int i = 0; i < obstacles.Length - 1; i++)
+                {
+                    if (ran < obstacleSpawnChances[i])
+                    {
+                        objToSpawn = obstacles[i];
+                        break;
+                    }
+                    ran -= obstacleSpawnChances[i];
+                }
+            }
+            else // obstacle
+            {
+                objToSpawn = obstacles[^1];
+                for (int i = 0; i < obstacles.Length - 1; i++)
+                {
+                    if (ran < obstacleSpawnChances[i])
+                    {
+                        objToSpawn = obstacles[i];
+                        break;
+                    }
+                    ran -= obstacleSpawnChances[i];
+                }
+            }
+            SpawnObject(objToSpawn, Random.Range(LevelController.Instance.roadSize, -LevelController.Instance.roadSize));
         }
     }
 }
