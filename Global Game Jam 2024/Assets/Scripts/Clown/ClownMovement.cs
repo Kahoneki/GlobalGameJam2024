eb36bc3171d7d3f769a284ecf0da0a2fea71a978
James Dobbie <38668771+Drogo01154@users.noreply.github.com>
2024-01-27 22:18:30 +0000

    Clown Animaton
    Added clown spinning animation and death animation

--- Global Game Jam 2024/Assets/Scripts/Clown/ClownMovement.cs
+++ Global Game Jam 2024/Assets/Scripts/Clown/ClownMovement.cs
@@ -7,8 +7,14 @@ public class ClownMovement : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
     [SerializeField] Transform tf;
+    [SerializeField] SpriteRenderer SR;
+    public Sprite deathSprite;
     float rotation;
     [SerializeField] int moveSpeed = 1000;
+    [SerializeField] float rotateSpeed = 10000f;
+    [SerializeField] int spinTimer = 1000;
+    bool spinning = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +24,24 @@ public class ClownMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(spinning)
+        {
+            rb.velocityY = (float)moveSpeed * Mathf.Sin(rotation) * LevelController.Instance.speedMultiplier * Time.deltaTime;
+            rb.angularVelocity = rotateSpeed * Time.deltaTime;
+        }
+        if(spinTimer <= 0)
+        {
+            spinning = false;
+        }
+        else
+        {
+            tf.rotation = Quaternion.Euler(0, 0, 0);
+            SR.sprite = deathSprite;
+            spinTimer--;
+        }
+        
         rb.velocityX = (float) moveSpeed * Mathf.Cos(rotation) * LevelController.Instance.speedMultiplier * Time.deltaTime;
-        rb.velocityY = (float) moveSpeed * Mathf.Sin(rotation) * LevelController.Instance.speedMultiplier * Time.deltaTime;
+        
         if ((tf.position.x < -2) || (tf.position.y < -6))
         {
             Debug.Log("Destroying Object");
