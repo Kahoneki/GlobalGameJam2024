aa9e0364d070250eeb4c72cca393029558637def
JOHN HICKEY <2302441@uad.ac.uk>
2024-01-28 12:02:01 +0000

    Another one

---
+++ Global Game Jam 2024/Assets/Score.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public LevelController clown;
+   [SerializeField] private TextMeshProUGUI counterText;
+    public float counter;
+    public string Agrade = "A";
+    public string Bgrade = "B";
+    public string Cgrade = "C";
+    public string Fail = "Fail";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (clown.livesLeft >= 9)
+        {
+            counterText.text = "A";
+        }
+       else  if (clown.livesLeft >= 7)
+        {
+            counterText.text = "B";
+        }
+       else if (clown.livesLeft >= 5)
+        {
+            counterText.text = "C";
+        }
+        else
+        {
+            counterText.text = "Pathetic";
+        }
+    }
+}
